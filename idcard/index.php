ded0513331e235d90f2912c1a448191b22f2dae5
ovarz <ovarz_03@yahoo.com>
2026-01-04 20:17:30 +0700

    base id card

--- idcard/index.php
+++ idcard/index.php
@@ -7,11 +7,14 @@
 <?php require ($_SERVER['IF'].'template/inc/sample.php')?>
 <?php require ($_SERVER['IF'].'template/inc/meta.php')?>
 <h1 class="hide"><?php echo $sitename; ?> <?php echo $menu; ?></h1>
-<div class="rancak-foundation">
+<div class="rancak-foundation content-center">
+  <div class="rancak-bg desktop-only">
+  
+  </div>
   
   
   
-  <section class="">
+  <section class="rancak-container">
   
 	tes
 	
